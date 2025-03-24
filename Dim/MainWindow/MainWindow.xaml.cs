3e961d933b9047fee8ddbe29207a509fb103c275
Sergei Kovalenko <smith404@live.com>
2025-03-24 23:23:01 +0100

    three buttons with some actions

---
+++ Dim/MainWindow/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Dim.MainWindow;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
