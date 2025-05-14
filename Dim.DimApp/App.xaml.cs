4de59725503c1a1815430b7c5e9a36c7deb226d5
Sergei Kovalenko <smith404@live.com>
2025-05-14 18:13:27 +0200

    Russian Culture initialized

---
+++ Dim.DimApp/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows;
+
+namespace Dim.DimApp;
+
+/// <summary>
+///     Interaction logic for App.xaml
+/// </summary>
+public partial class App
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        // Set culture
+        var culture = new CultureInfo("ru-RU"); // Replace with your desired culture (e.g., "de-DE", "en-US")
+
+        Thread.CurrentThread.CurrentCulture = culture;
+        Thread.CurrentThread.CurrentUICulture = culture;
+
+        base.OnStartup(e);
+    }
+}
--- Dim.DimApp/MainWindow.xaml.cs
+++ Dim.DimApp/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
-﻿namespace Dim.MainWindow;
+﻿namespace Dim.DimApp;
 
 /// <summary>
-/// Interaction logic for MainWindow.xaml
+///     Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow
 {
